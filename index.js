a3021e751c4c2329ec321938a810577dcb3c9a69
idotz <95885666+idotz@users.noreply.github.com>
2023-12-31 03:25:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -35,114 +34,49 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-var playwright_extra_1 = require("playwright-extra");
+var _this = this;
+var firefox = require("playwright-extra").firefox;
 var stealth = require('puppeteer-extra-plugin-stealth')();
-// let file = fs.readFileSync("pwoxy.txt")
-// let lines = file.toString().split("\n")
+var delay = function (ms) { return new Promise(function (resolve) { return setTimeout(resolve, ms * 1000); }); };
+var express = require("express");
+// chromium.use(stealth)
 var _a = require('discord-webhook-node'), Webhook = _a.Webhook, MessageBuilder = _a.MessageBuilder;
 var hook = new Webhook("https://canary.discord.com/api/webhooks/1190831794157789194/7woA16J43vhLVfh9sXAv2390jfJxTB_OGe3fWOlweP_F5b7-XFkAcSO2Z3XPut3m0lrb");
-function makeid(length) {
-    var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    var charactersLength = characters.length;
-    var counter = 0;
-    while (counter < length) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        counter += 1;
-    }
-    return result;
-}
-;
-var ms = require("mailjs-gw");
-var time = 0;
-var delay = function (ms) { return new Promise(function (resolve) { return setTimeout(resolve, ms * 1000); }); };
-var _loop_1 = function (i) {
-    time += 5000;
-    setTimeout(function () {
-        owo(i);
-    }, time);
-};
-for (var i = 0; i < 1; i++) {
-    _loop_1(i);
-}
-;
-function owo(ihg) {
-    return __awaiter(this, void 0, void 0, function () {
-        var mailjs, emil, folderid, browser, zz;
-        var _this = this;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    console.log(ihg);
-                    mailjs = new ms();
-                    return [4 /*yield*/, mailjs.createOneAccount()
-                        // await mailjs.login("dvueo@hexv.com", "5aquhsvi")
-                        // console.log(emil)
-                        // return
-                        // return;
-                    ];
-                case 1:
-                    emil = _a.sent();
-                    folderid = makeid(14);
-                    return [4 /*yield*/, playwright_extra_1.webkit.launch({
-                            headless: false,
-                        })];
-                case 2:
-                    browser = _a.sent();
-                    zz = function () { return __awaiter(_this, void 0, void 0, function () {
-                        var x, err_1, xd, err_2;
-                        return __generator(this, function (_a) {
-                            switch (_a.label) {
-                                case 0: return [4 /*yield*/, browser.newPage()];
-                                case 1:
-                                    x = _a.sent();
-                                    return [4 /*yield*/, x.goto("https://youtu.be/U3oEvGpHRz8")];
-                                case 2:
-                                    _a.sent();
-                                    return [4 /*yield*/, delay(20)];
-                                case 3:
-                                    _a.sent();
-                                    _a.label = 4;
-                                case 4:
-                                    _a.trys.push([4, 6, , 7]);
-                                    return [4 /*yield*/, x.locator("xpath=/html/body/ytd-app/ytd-consent-bump-v2-lightbox/tp-yt-paper-dialog/div[4]/div[2]/div[6]/div[1]/ytd-button-renderer[1]/yt-button-shape/button/yt-touch-feedback-shape/div/div[2]").click()];
-                                case 5:
-                                    _a.sent();
-                                    return [3 /*break*/, 7];
-                                case 6:
-                                    err_1 = _a.sent();
-                                    return [3 /*break*/, 7];
-                                case 7:
-                                    _a.trys.push([7, 11, , 12]);
-                                    return [4 /*yield*/, x.locator("xpath=/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[3]/div[1]/div[2]/ytd-player/div/div/div[30]/div[2]/div[1]/button").getAttribute("data-title-no-tooltip")];
-                                case 8:
-                                    xd = _a.sent();
-                                    if (!(xd == "Play")) return [3 /*break*/, 10];
-                                    return [4 /*yield*/, x.locator("xpath=/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[3]/div[1]/div[2]/ytd-player/div/div/div[30]/div[2]/div[1]/button").click()];
-                                case 9:
-                                    _a.sent();
-                                    _a.label = 10;
-                                case 10: return [3 /*break*/, 12];
-                                case 11:
-                                    err_2 = _a.sent();
-                                    return [3 /*break*/, 12];
-                                case 12: return [4 /*yield*/, delay(30)];
-                                case 13:
-                                    _a.sent();
-                                    return [4 /*yield*/, x.screenshot({ path: 'screenshot.png' })];
-                                case 14:
-                                    _a.sent();
-                                    hook.sendFile('./screenshot.png');
-                                    return [2 /*return*/];
-                            }
-                        });
-                    }); };
-                    zz();
-                    return [2 /*return*/];
-            }
-        });
+// chromium.use(stealth);
+var app = express();
+app.get("/", function (req, res) {
+    res.send("hi");
+});
+var exec = require("child_process").exec;
+exec("which firefox", function (error, stdout, stderr) { return __awaiter(_this, void 0, void 0, function () {
+    var browser, page, err_1;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, firefox.launch({ headless: false, executablePath: stdout.split("\n")[0], args: ['--no-sandbox'] })];
+            case 1:
+                browser = _a.sent();
+                return [4 /*yield*/, browser.newPage()];
+            case 2:
+                page = _a.sent();
+                return [4 /*yield*/, page.goto("https://youtu.be/U3oEvGpHRz8")];
+            case 3:
+                _a.sent();
+                _a.label = 4;
+            case 4:
+                _a.trys.push([4, 6, , 7]);
+                return [4 /*yield*/, page.locator("xpath=/html/body/ytp-app/div[1]/ytp-page-manager/ytp-watch-flexy/div[5]/div[1]/div/div[1]/div[2]/div/div/ytp-player/div/div/div[5]/button").click()];
+            case 5:
+                _a.sent();
+                return [3 /*break*/, 7];
+            case 6:
+                err_1 = _a.sent();
+                return [3 /*break*/, 7];
+            case 7: return [4 /*yield*/, page.screenshot("x.png")];
+            case 8:
+                _a.sent();
+                hook.sendFile('./screenshot.png');
+                return [2 /*return*/];
+        }
     });
-}
-// https://bitbucket.org/workspaces/ybtobdgthlya/avatar/?ts=1698787745
-//https://secure.gravatar.com/avatar/60391a39f09db985663445d220721e7d?d=https%3A%2F%2Favatar-management--avatars.us-west-2.prod.public.atl-paas.net%2Finitials%2FF-5.png
+}); });
+app.listen("8080");
