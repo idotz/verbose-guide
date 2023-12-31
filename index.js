bf6391e4ed0bf50c8c2655f3dd9cc1f4cef12cf0
idotz <95885666+idotz@users.noreply.github.com>
2024-01-01 00:08:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ function owo(ihg) {
                                 var err_1;
                                 return __generator(this, function (_a) {
                                     switch (_a.label) {
-                                        case 0: return [4 /*yield*/, x.goto("https://www.youtube.com/watch?v=VpStE1cEFqQ")];
+                                        case 0: return [4 /*yield*/, x.goto("https://www.youtube.com/@ToolsCracks/live")];
                                         case 1:
                                             _a.sent();
                                             return [4 /*yield*/, delay(20)];
